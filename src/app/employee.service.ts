7002ad348d97615b467291d3fced1bf41052852b
Iraz Irfan <irazirfan@gmail.com>
2021-01-26 23:18:48 +0600

    employee component, service, class & service method added

---
+++ src/app/employee.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  url = 'http://localhost:44326/Api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  getAllEmployee(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.url + '/GetEmployees');
+  }
+
+  getEmployeeById(id: number): Observable<Employee> {
+    return this.http.get<Employee>(this.url + '/GetEmployeeById/' + id);
+  }
+
+  createEmployee(employee: Employee): Observable<Employee> {
+    const httpOptions = {headers: new HttpHeaders({ 'content-type': 'application/json'})}
+    return this.http.post<Employee>(this.url + '/AddEmployee', employee, httpOptions);
+  }
+
+  updateEmployee(employee: Employee): Observable<Employee> {
+    const httpOptions = {headers: new HttpHeaders({ 'content-type': 'application/json'})}
+    return this.http.put<Employee>(this.url + '/UpdateEmployee', employee, httpOptions);
+  }
+
+  deleteEmployeeById(id: number): Observable<number> {
+    const httpOptions = {headers: new HttpHeaders({ 'content-type': 'application/json'})}
+    return this.http.delete<number>(this.url + '/DeleteEmployee?id=' + id, httpOptions);
+  }
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    Id: number;
+    Name: string;
+    Email: string;
+    Address: string;
+    Phone: string;
+}
